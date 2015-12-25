76653bf25338ba42d74270f619565fc278d46ac7
eray <relfishere@gmail.com>
2015-12-25 16:44:33 +0200

    fix.

--- server.js
+++ server.js
@@ -8,16 +8,27 @@ var sockets = {};
 app.use(express.static('public'));
 
 io.on('connection', function (socket) {
+  // Spawn for all Players
   io.emit("spawn", {id: socket.id});
+
+  // Already on Map Players Spawn for Me
   for (var i in sockets) {
-    socket.emit("spawn", {id: sockets[i].id, x: sockets[i]._x, y: sockets[i]._y})
+    socket.emit("spawn", {
+      id: sockets[i].id,
+      x: sockets[i]._x,
+      y: sockets[i]._y
+    });
   }
+
+  // Register Socket Object
   sockets[socket.id] = socket;
 
+  // Handle Move Action
   socket.on("move", function (data) {
     socket._x = data.x;
     socket._y = data.y;
 
+    // Broadcast Move Action for Another Players
     socket.broadcast.emit("move", {
       x: data.x,
       y: data.y,
@@ -25,8 +36,11 @@ io.on('connection', function (socket) {
     });
   });
 
+  // Handle Disconnect Event
   socket.on('disconnect', function () {
     delete sockets[socket.id];
+
+    // Destroy for all Players
     io.emit("destroy", {id: socket.id});
   });
 });
